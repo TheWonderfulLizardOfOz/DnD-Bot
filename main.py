487b5a62a317c4fd3c5f4a80111ee54f061de352
TheWonderfulLizardOfOz <garancear@gmail.com>
2021-07-18 22:39:56 +0100

    added the extensions, and on ready message

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import tasks, commands
+from dotenv import load_dotenv
+load_dotenv(".env")
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix='!')
+
+extentions = ["cogs.backstory", "cogs.owner"]
+
+@bot.event
+async def on_ready():
+    print("bot is ready for stuff")
+
+if __name__ == "__main__":
+    for extension in extentions:
+        bot.load_extension(extension)
+
+bot.run(TOKEN)
