0b7c082505741d777dfeacfe0d281fed79f7b67e
TheWonderfulLizardOfOz <garancear@gmail.com>
2021-07-18 22:38:30 +0100

    only the owner of the server can load and unload clogs for the bot, this is a relatively useless feature due to there only being one cog to load and unload but will be more useful in future if the server owner wants a cog to be disabled

---
+++ cogs/owner.py
@@ -0,0 +1,39 @@
+from discord.ext import commands
+
+class Owner(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="load")
+    @commands.is_owner()
+    async def load_cog(self, ctx, arg):
+        try:
+            self.bot.load_extension(arg)
+        except Exception as e:
+            await ctx.send(f"**`Error`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send("**`Success`**")
+
+    @commands.command(name = "unload")
+    @commands.is_owner()
+    async def unload_cog(self, ctx, arg):
+        try:
+            self.bot.unload_extension(arg)
+        except Exception as e:
+            await ctx.send(f"**`Error`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send("**`Success`**")
+
+    @commands.command(name = "reload")
+    @commands.is_owner()
+    async def reload_cog(self, ctx, arg):
+        try:
+            self.bot.unload_extension(arg)
+            self.bot.load_extension(arg)
+        except Exception as e:
+            await ctx.send(f"**`Error`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send("**`Success`**")
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
