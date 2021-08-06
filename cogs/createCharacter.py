b6f698f5c8c21806507d0a9d95e9d0a25cb2f8ce
TheWonderfulLizardOfOz <garancear@gmail.com>
2021-08-07 00:53:54 +0100

    added race and race features to !createCharacter

--- cogs/createCharacter.py
+++ cogs/createCharacter.py
@@ -1,10 +1,17 @@
 from discord.ext import commands
 from cogs.background import *
 from cogs.dice import *
+import os.path
+import json
 
 class CreateCharacter(Background, Roll):
     def __init__(self, bot):
         super().__init__(bot)
+        with open(os.path.dirname(__file__) + "/../races.json", "r") as jsonFile:
+            self.raceDict = json.load(jsonFile)
+        self.race = ""
+        self.stats = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+        self.racialTraits = []
 
     @commands.command()
     async def createCharacter(self, ctx):
@@ -15,22 +22,55 @@ class CreateCharacter(Background, Roll):
         self.setBond()
         self.setFlaw()
         self.getLanguagesNo()
+        self.nums = self.rollDice(6, 20)
+        self.setRace()
         languages = self.setLanguages()
         self.setLanguageMessage(languages)
         self.setName()
         backMessage = self.message()
+        statMessage = self.messageCreator()
+        raceMessage = self.createRaceMessage()
+        await ctx.send(backMessage + "\n" + statMessage + raceMessage)
 
-        nums = self.rollDice(6, 20)
-        statMessage = self.messageCreator(nums)
+    def setRace(self):
+        raceList = []
+        for race in self.raceDict:
+            raceList.append(race)
+        self.race = random.choice(raceList)
+        raceAttributes = self.raceDict[self.race]
+        self.addRandBonus(raceAttributes)
+        for i in range(len(self.stats)):
+            self.nums[i] += raceAttributes.get(self.stats[i], 0)
+        self.noLanguages += raceAttributes.get("languages bonus", 0)
+        self.setRacialTraits(raceAttributes)
 
-        await ctx.send(backMessage + "\n" + statMessage)
+    def addRandBonus(self, raceAttributes):
+        randBonus = raceAttributes.get("random bonus")
+        if randBonus is not None:
+            for i in range(randBonus[0]):
+                statToAdd = random.choice(self.stats)
+                if raceAttributes.get(statToAdd, 0) == 0:
+                    statIndex = self.stats.index(statToAdd)
+                    self.nums[statIndex] += randBonus[1]
 
-    def messageCreator(self, nums):
+    def setRacialTraits(self, raceAttributes):
+        self.racialTraits = []
+        for trait in raceAttributes["traits"]:
+            self.racialTraits.append(trait)
+
+    def messageCreator(self):
         message = ""
-        stats = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
-        for i in range(len(stats)):
-            message += "**`" + stats[i] + ":`** " + str(nums[i]) +"\n"
+        for i in range(len(self.stats)):
+            message += "**`" + self.stats[i].title() + ":`** " + str(self.nums[i]) +"\n"
         return message
+
+    def createRaceMessage(self):
+        message = "**`Race:`** " + self.race + "\n**`Racial Traits:`** "
+        for trait in self.racialTraits:
+            message += trait + ", "
+        message = message[:-2]
+        return message
+
 
 def setup(bot):
     bot.add_cog(CreateCharacter(bot))
